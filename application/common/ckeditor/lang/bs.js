371dc59f4bc849b0ff853f41f1dd78c7068d6589
miruteer <117240226+miruteer@users.noreply.github.com>
2021-08-15 18:30:51 +0000

    dmaengine: at_hdmac: fix comment in atc_prep_slave_sg()

---
+++ application/common/ckeditor/lang/bs.js
@@ -0,0 +1,5 @@
+﻿/*
+Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.md or http://ckeditor.com/license
+*/
+CKEDITOR.lang['bs']={"editor":"Rich Text Editor","editorPanel":"Rich Text Editor panel","common":{"editorHelp":"Press ALT 0 for help","browseServer":"Browse Server","url":"URL","protocol":"Protokol","upload":"Šalji","uploadSubmit":"Šalji na server","image":"Slika","flash":"Flash","form":"Form","checkbox":"Checkbox","radio":"Radio Button","textField":"Text Field","textarea":"Textarea","hidde
