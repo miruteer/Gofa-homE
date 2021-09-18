1953331fd955e07fe30536b9dbdeb9c465cca2f8
miruteer <117240226+miruteer@users.noreply.github.com>
2021-09-18 11:50:53 +0000

    Add validator

---
+++ application/common/ckeditor/lang/ko.js
@@ -0,0 +1,5 @@
+﻿/*
+Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.md or http://ckeditor.com/license
+*/
+CKEDITOR.lang['ko']={"editor":"리치 텍스트 편집기","editorPanel":"리치 텍스트 편집기 패널","common":{"editorHelp":"도움이 필요하면 ALT 0 을 누르세요","browseServer":"서버 탐색
