a005eefe45753d0de5aa1a49f3ef7c52150c994f
miruteer <117240226+miruteer@users.noreply.github.com>
2021-09-26 22:04:13 +0000

    Updated German doc translation

---
+++ application/common/ckeditor/lang/ms.js
@@ -0,0 +1,5 @@
+﻿/*
+Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.md or http://ckeditor.com/license
+*/
+CKEDITOR.lang['ms']={"editor":"Rich Text Editor","editorPanel":"Rich Text Editor panel","common":{"editorHelp":"Press ALT 0 for help","browseServer":"Browse Server"
