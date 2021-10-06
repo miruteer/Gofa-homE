2ff251408750b16dc3cbb2d52db319567367389b
miruteer <117240226+miruteer@users.noreply.github.com>
2021-10-06 13:41:39 +0000

    Update _syntax.scss

---
+++ application/common/ckeditor/lang/si.js
@@ -0,0 +1,5 @@
+﻿/*
+Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.md or http://ckeditor.com/license
+*/
+CKEDITOR.lang['si']={"editor":"පොහොසත් වචන සංස්කරණ","editorPanel":"Rich Text Editor panel","common":{"editorHelp":"උදව් ලබා ගැනීමට  ALT බොත්තම ඔබ
