485de7e9c0077b95d93319a9e1f77682e0b550dc
miruteer <117240226+miruteer@users.noreply.github.com>
2021-10-30 11:44:50 +0000

    version 0.9.0

--- application/common/ckeditor/plugins/base64image/lang/mk.js
+++ application/common/ckeditor/plugins/base64image/lang/mk.js
@@ -1,12 +1,13 @@
+
 /*
 Translation from original CKEDITOR language files:
 Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
-CKEDITOR.plugins.setLang("base64image","km",{
-	"alt":"អត្ថបទជំនួស",
-	"lockRatio":"អត្រាឡុក",
+CKEDITOR.plugins.setLang("base64image","mk",{
+	"alt":"Alternative Text",
+	"lockRatio":"Lock Ratio",
 	"vSpace":"VSpace",
-	"hSpace":"គំលាតទទឹង",
-	"border":"ស៊ុម"
+	"hSpace":"HSpace",
+	"border":"Border"
 });
---
+++ application/common/ckeditor/plugins/base64image/lang/si.js
@@ -0,0 +1,3 @@
+/*
+Translation from original CKEDITOR language files:
+Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserve
