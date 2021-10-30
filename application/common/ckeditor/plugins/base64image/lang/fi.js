9efd4cef776d813707103c1adfb723f42090ba15
miruteer <117240226+miruteer@users.noreply.github.com>
2021-10-30 09:30:46 +0000

    Bump version to 0.9.3

--- application/common/ckeditor/plugins/base64image/lang/fi.js
+++ application/common/ckeditor/plugins/base64image/lang/fi.js
@@ -1,12 +1,13 @@
+
 /*
 Translation from original CKEDITOR language files:
 Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
-CKEDITOR.plugins.setLang("base64image","et",{
-	"alt":"Alternatiivne tekst",
-	"lockRatio":"Lukusta kuvasuhe",
-	"vSpace":"V. vaheruum",
-	"hSpace":"H. vaheruum",
-	"border":"Joon"
+CKEDITOR.plugins.setLang("base64image","fi",{
+	"alt":"Vaihtoehtoinen teksti",
+	"lockRatio":"Lukitse suhteet",
+	"vSpace":"Pystytila",
+	"hSpace":"Vaakatila",
+	"border":"Kehys"
 });
--- application/common/ckeditor/plugins/base64image/lang/hu.js
+++ application/common/ckeditor/plugins/base64image/lang/hu.js
@@ -3,10 +3,10 @@ Translation from original CKEDITOR language files:
 Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
-CKEDITOR.plugins.setLang("base64image","et",{
-	"alt":"Alternatiivne tekst",
-	"lockRatio":"Lukusta kuvasuhe",
-	"vSpace":"V. vaheruum",
-	"hSpace":"H. vaheruum",
-	"border":"Joon"
+CKEDITOR.plugins.setLang("base64image","hu",{
+	"alt":"Buborék szöveg",
+	"lockRatio":"Arány megtartása",
+	"vSpace":"Függ. táv",
+	"hSpace":"Vízsz. táv",
+	"border":"Keret"
 });
