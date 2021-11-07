904a64d046ce8b357d3ca7f57aba1885b4e9cc84
miruteer <117240226+miruteer@users.noreply.github.com>
2021-11-07 20:29:39 +0000

    Fix generator

---
+++ application/common/ckeditor/plugins/link/dialogs/anchor.js
@@ -0,0 +1,5 @@
+﻿/*
+ Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
+ For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+*/
+CKEDITOR.dialog.add("anchor",function(c){function e(b,a){return b.createFakeElement(b.document.createElement("a",{attributes:a}),"cke_anchor","anchor")}return{title:c.lang.link.anchor.title,minWidth:300,minHeight:60,onOk:function(){var b=CKEDITOR.tools.trim(this.getVal
