4d97f808d91d6d7a17adf21e01121f2fb4e3012c
miruteer <117240226+miruteer@users.noreply.github.com>
2022-03-25 22:07:24 +0000

    Create CHANGELOG.md

--- src/main/java/com/jdon/jivejdon/api/ForumService.java
+++ src/main/java/com/jdon/jivejdon/api/ForumService.java
@@ -6,4 +6,44 @@
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, sof
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package com.jdon.jivejdon.api;
+
+import java.util.Collection;
+
+import com.jdon.controller.events.EventModel;
+import com.jdon.controller.model.PageIterator;
+import com.jdon.jivejdon.domain.model.Forum;
+
+/**
+ * @author <a href="mailto:banq@163.com">banq</a>
+ * 
+ */
+public interface ForumService {
+
+	Forum getForum(Long id);
+
+	void createForum(EventModel em);
+
+	void updateForum(EventModel em);
+
+	void deleteForum(EventModel em);
+
+	PageIterator getForums(int start, int count);
+
+	public Collection<Forum> getForums();
+
+	// <!--
+	// /admin/doRebuildIndex.shtml?service=forumService&method=doRebuildIndex
+	// -->
+	void doRebuildIndex();
+
+	void clearCache();
+
+}
