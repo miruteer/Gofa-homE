77a481a960954a2a2e58004b44eba09907f0e4b8
miruteer <117240226+miruteer@users.noreply.github.com>
2022-04-05 16:28:43 +0000

    Address PR feedback

--- src/main/java/com/jdon/jivejdon/api/impl/shortmessage/ShortMessageServiceImp.java
+++ src/main/java/com/jdon/jivejdon/api/impl/shortmessage/ShortMessageServiceImp.java
@@ -72,4 +72,148 @@ public class ShortMessageServiceImp implements ShortMessageService {
 	public ShortMessageServiceImp(SessionContextUtil sessionContextUtil, ShortMessageRepository repository, ShortMessageFactory factory,
 			AccountFactory accountFactory, ForumAbstractFactory forumAbstractFactory) {
 		this.sessionContextUtil = sessionContextUtil;
-		this.repository =
+		this.repository = repository;
+		this.factory = factory;
+		this.accountFactory = accountFactory;
+		this.forumAbstractFactory = forumAbstractFactory;
+	}
+
+	/**
+	 * 发送草稿箱中的消息
+	 * 
+	 * @throws Exception
+	 * 
+	 */
+	public void sendInDraftMessage(EventModel em) {
+		logger.debug("ShortMessageServiceImp");
+		try {
+			FromShortMessage msg = (FromShortMessage) em.getModelIF();
+			if (userIsExists(msg.getMessageTo()) == null)
+				throw new Exception("No such user!");
+			// 这里发送的是已经写的消息，这里只是更新表中的数据
+			msg.getShortMessageState().setHasSent(true);
+			this.repository.updateShortMessage(msg);
+		} catch (Exception e) {
+			em.setErrors("发送的对象不存在，请检查拼写！");
+		}
+	}
+
+	/**
+	 * 发送草稿箱中的消息
+	 * 
+	 * @throws Exception
+	 * 
+	 */
+	public void saveInDraftMessage(EventModel em) {
+		logger.debug("ShortMessageServiceImp");
+		try {
+			FromShortMessage msg = (FromShortMessage) em.getModelIF();
+			if (userIsExists(msg.getMessageTo()) == null)
+				throw new Exception("No such user!");
+			// 这里发送的是已经写的消息，这里只是更新表中的数据
+			this.repository.updateShortMessage(msg);
+		} catch (Exception e) {
+			em.setErrors("发送的对象不存在，请检查拼写！");
+		}
+	}
+
+	/**
+	 * 删除草稿箱中的消息
+	 * 
+	 * @throws Exception
+	 * 
+	 */
+	public void deleInDraftMessage(EventModel em) {
+		logger.debug("ShortMessageServiceImp");
+		try {
+			deleteShortMessage(em);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			em.setErrors("删除操作出错！");
+		}
+	}
+
+	/**
+	 * 发送 新写的 消息 这是往表里增加新的数据
+	 * 
+	 */
+	public void sendShortMessage(EventModel em) {
+		logger.debug("ShortMessageServiceImp");
+		try {
+			FromShortMessage msg = (FromShortMessage) em.getModelIF();
+			if (userIsExists(msg.getMessageTo()) == null)
+				throw new Exception("No such user!");
+			Account account = this.sessionContextUtil.getLoginAccount(sessionContext);
+			msg.setAccount(account);
+			msg.setMessageFrom(account.getUsername());
+			if (!this.factory.sendShortMessage(msg))
+				em.setErrors("shormessage.exceed.max");
+		} catch (Exception e) {
+			em.setErrors("errors.nouser");
+		}
+	}
+
+	/**
+	 * 保存消息
+	 */
+	public void saveShortMessage(EventModel em) {
+		logger.debug("service: saveShortMessage()");
+		try {
+			FromShortMessage msg = (FromShortMessage) em.getModelIF();
+			if (userIsExists(msg.getMessageTo()) == null)
+				throw new Exception("No such user!");
+			Account account = this.sessionContextUtil.getLoginAccount(sessionContext);
+			msg.setAccount(account);
+			if (!this.factory.saveShortMessage(msg))
+				em.setErrors("shormessage.exceed.max");
+		} catch (Exception e) {
+			em.setErrors("errors.nouser");
+		}
+	}
+
+	/**
+	 * 
+	 * @param userId
+	 * @throws Exception
+	 */
+	private Account userIsExists(String userName) throws Exception {
+		return factory.findTheUser(userName);
+	}
+
+	/**
+	 * 删除消息
+	 * 
+	 * @throws Exception
+	 */
+	public void deleteShortMessage(EventModel em) throws Exception {
+		logger.debug("deleteShortMessage");
+		ShortMessage smf = (FromShortMessage) em.getModelIF();
+		logger.debug("" + smf.getMsgId());
+		smf = this.factory.findShortMessage(smf.getMsgId());
+		Account loginaccount = this.sessionContextUtil.getLoginAccount(sessionContext);
+		// check auth
+		if ((smf.getMessageFrom().equals(loginaccount.getUsername())) || (smf.getMessageTo().equals(loginaccount.getUsername()))) {
+			this.repository.deleteShortMessage(smf);
+			loginaccount.reloadAccountSMState();
+		}
+	}
+
+	public void deleteUserAllShortMessage() throws Exception {
+		logger.debug("deleteShortMessage");
+		Account loginaccount = this.sessionContextUtil.getLoginAccount(sessionContext);
+		this.repository.deleteUserAllShortMessage(loginaccount.getUserId());
+		loginaccount.reloadAccountSMState();
+	}
+
+	public void deleteUserRecAllShortMessage() throws Exception {
+		logger.debug("deleteUserRecAllShortMessage");
+		Account loginaccount = this.sessionContextUtil.getLoginAccount(sessionContext);
+		this.repository.deleteUserRecAllShortMessage(loginaccount.getUsername());
+		loginaccount.reloadAccountSMState();
+	}
+
+	/**
+	 * 编辑消息
+	 */
+	public void updateShortMessage(EventModel em) {
+		logger.debug("Sho
