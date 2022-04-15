b5cf037ded2fe77c4c95ab2f70fbf852abb45683
miruteer <117240226+miruteer@users.noreply.github.com>
2022-04-15 11:50:43 +0000

    Version bump to 1.1.0.rc3

--- src/main/java/com/jdon/jivejdon/domain/model/attachment/UploadFile.java
+++ src/main/java/com/jdon/jivejdon/domain/model/attachment/UploadFile.java
@@ -9,4 +9,67 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See t
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package com.jdon.jivejdon.domain.model.attachment;
+
+import com.jdon.jivejdon.domain.model.ForumMessage;
+import com.jdon.jivejdon.domain.model.message.upload.UploadHelper;
+
+/**
+ * @author banq(http://www.jdon.com)
+ * 
+ */
+
+public class UploadFile extends com.jdon.strutsutil.file.UploadFile implements Comparable {
+	private static final long serialVersionUID = 1L;
+
+	private UploadHelper uploadHelper;
+
+	// private UploadCacheMonitored ucm;
+
+	public UploadFile() {
+		this.uploadHelper = new UploadHelper();
+		// this.ucm = new UploadCacheMonitored();
+	}
+
+	public void addMonitor(ForumMessage message) {
+		// this.ucm.addObserver(message);
+	}
+
+	public boolean isImage() {
+		return uploadHelper.isImage(this.getName());
+
+	}
+
+	public byte[] getData(String oid) {
+		if (!isValid(Integer.parseInt(oid)))
+			return null;
+		return getData();
+	}
+
+	public boolean isValid(int oid) {
+		if (oid == Integer.parseInt(this.getParentId())) {
+			return true;
+		}
+		return false;
+	}
+
+	// // parent id
+	public int getOid() {
+		try {
+			return Integer.parseInt(this.getParentId());
+		} catch (NumberFormatException e) {
+			return 0;
+		}
+	}
+
+	public int compareTo(Object comparesup) {
+		long compareid = Long.parseLong(((UploadFile) comparesup).getId());
+		long thisid = Long.parseLong(this.getId());
+		return Long.compare(thisid, compareid);
+
+	}
+}
