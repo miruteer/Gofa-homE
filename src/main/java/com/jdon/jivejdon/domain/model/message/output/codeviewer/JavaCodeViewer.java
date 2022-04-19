ec326f3fa307d9b260d92bc15fd020dab2589576
miruteer <117240226+miruteer@users.noreply.github.com>
2022-04-19 09:29:16 +0000

    Repackaging

--- src/main/java/com/jdon/jivejdon/domain/model/message/output/codeviewer/JavaCodeViewer.java
+++ src/main/java/com/jdon/jivejdon/domain/model/message/output/codeviewer/JavaCodeViewer.java
@@ -22,4 +22,148 @@ import java.util.HashMap;
  * 
  * A class that syntax highlights Java code into html.
  * <p>
- * A CodeViewer 
+ * A CodeViewer object is used to syntax highlight Java code. To make use of
+ * this class, first set up how tokens should be highlighted by invoking the
+ * getXXX and setXXX methods, then pass in "chunks" of relevent input to be
+ * filtered. Typically this will mean the section(s) of a form input that has
+ * been tagged with [code][/code] tags.
+ */
+public class JavaCodeViewer {
+	private static HashMap reservedWords = new HashMap(150); // >= Java2 only
+																// (also, not
+																// thread-safe)
+	private char[] backgroundColor = "#ffffff".toCharArray();
+	private char[] commentStart = "<font color=\"darkgreen\">".toCharArray();
+	private char[] commentEnd = "</font>".toCharArray();
+	private char[] stringStart = "<font color=\"red\">".toCharArray();
+	private char[] stringEnd = "</font>".toCharArray();
+	private char[] reservedWordStart = "<font color=\"navy\"><b>".toCharArray();
+	private char[] reservedWordEnd = "</b></font>".toCharArray();
+	private char[] methodStart = "<font color=\"brown\">".toCharArray();
+	private char[] methodEnd = "</font>".toCharArray();
+	private char[] characterStart = "<font color=\"navy\">".toCharArray();
+	private char[] characterEnd = "</font>".toCharArray();
+	private char[] bracketStart = "<font color=\"navy\">".toCharArray();
+	private char[] bracketEnd = "</font>".toCharArray();
+	private char[] numberStart = "<font color=\"red\">".toCharArray();
+	private char[] numberEnd = "</font>".toCharArray();
+	private char[] nbsp = "&nbsp;".toCharArray();
+	private char stringEscape = '\\';
+	private char characterEscape = '\\';
+	private boolean filterMethod = false;
+	private boolean filterNumber = false;
+
+	/**
+	 * Load all keywords at class loading time.
+	 */
+	static {
+		loadKeywords();
+	}
+
+	/**
+	 * Gets the html for the start of a comment block.
+	 */
+	public String getCommentStart() {
+		return String.valueOf(commentStart);
+	}
+
+	/**
+	 * Sets the html for the start of a comment block.
+	 */
+	public void setCommentStart(String commentStart) {
+		this.commentStart = commentStart.toCharArray();
+	}
+
+	/**
+	 * Gets the html for the end of a comment block.
+	 */
+	public String getCommentEnd() {
+		return String.valueOf(commentEnd);
+	}
+
+	/**
+	 * Sets the html for the end of a comment block.
+	 */
+	public void setCommentEnd(String commentEnd) {
+		this.commentEnd = commentEnd.toCharArray();
+	}
+
+	/**
+	 * Gets the html for the start of a String.
+	 */
+	public String getStringStart() {
+		return String.valueOf(stringStart);
+	}
+
+	/**
+	 * Sets the html for the start of a String.
+	 */
+	public void setStringStart(String stringStart) {
+		this.stringStart = stringStart.toCharArray();
+	}
+
+	/**
+	 * Gets the html for the end of a String.
+	 */
+	public String getStringEnd() {
+		return String.valueOf(stringEnd);
+	}
+
+	/**
+	 * Sets the html for the end of a String.
+	 */
+	public void setStringEnd(String stringEnd) {
+		this.stringEnd = stringEnd.toCharArray();
+	}
+
+	/**
+	 * Gets the html for the start of a reserved word.
+	 */
+	public String getReservedWordStart() {
+		return String.valueOf(reservedWordStart);
+	}
+
+	/**
+	 * Sets the html for the start of a reserved word.
+	 */
+	public void setReservedWordStart(String reservedWordStart) {
+		this.reservedWordStart = reservedWordStart.toCharArray();
+	}
+
+	/**
+	 * Gets the html for the end of a reserved word.
+	 */
+	public String getReservedWordEnd() {
+		return String.valueOf(reservedWordEnd);
+	}
+
+	/**
+	 * Sets the html for the end of a reserved word.
+	 */
+	public void setReservedWordEnd(String reservedWordEnd) {
+		this.reservedWordEnd = reservedWordEnd.toCharArray();
+	}
+
+	/**
+	 * Gets the html for the start of a method.
+	 */
+	public String getMethodStart() {
+		return String.valueOf(methodStart);
+	}
+
+	/**
+	 * Sets the html for the start of a method.
+	 */
+	public void setMethodStart(String methodStart) {
+		this.methodStart = methodStart.toCharArray();
+	}
+
+	/**
+	 * Gets the html for the end of a method.
+	 */
+	public String getMethodEnd() {
+		return String.valueOf(methodEnd);
+	}
+
+	/**
+	 * Sets the
