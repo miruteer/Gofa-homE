30f2700442624a6ca408f3f769fc3b2e2cbbf7c5
miruteer <117240226+miruteer@users.noreply.github.com>
2022-05-06 19:05:46 +0000

    merging staging into dev

--- src/main/java/com/jdon/jivejdon/domain/model/message/output/html/QuoteFilter.java
+++ src/main/java/com/jdon/jivejdon/domain/model/message/output/html/QuoteFilter.java
@@ -194,4 +194,161 @@ public class QuoteFilter implements Function<MessageVO, MessageVO> {
 	 * <li>Restoring the text format as close as possible to the original
 	 * formatting of the text prior to being reformatted by a MUA such as
 	 * Microsoft Outlook. MUA's often wrap lines of length > 76 characters
-	 * regardless of whether they are quote
+	 * regardless of whether they are quoted or not.</li>
+	 * </ul>
+	 * 
+	 * @param lines
+	 *            an arraylist of strings, one line/arraylist entry
+	 */
+	private void reformatText(ArrayList lines) {
+		char chars[];
+		int count;
+		int readAhead;
+		boolean isHTMLFilterActive = false;
+
+		for (int c = 0; c < lines.size(); c++) {
+			chars = getLineAsChar(lines, c);
+
+			// if this isn't a quoted line, skip it
+			if (!isQuoted(chars)) {
+				continue;
+			}
+
+			// check to see if we substituted any &gt; text
+			if (chars.length < ((String) lines.get(c)).length()) {
+				isHTMLFilterActive = true;
+			}
+
+			count = countQuotes(chars);
+			readAhead = (lines.size() <= c + count + 1) ? lines.size() : c + count + 1;
+			LinkedList l = new LinkedList();
+
+			// For each quoted line, read ahead (n-1 quotes) lines to see
+			// if the following lines are actually wrapped text from the
+			// current quoted line.
+			//
+			// Notes: - If any of the following lines do not have < n quotes
+			// (no quote acceptable), they are not wrapped text. However
+			// if prior following lines fulfill all the other criteria,
+			// they are wrapped text
+			// - If any of the following lines have more than 2 words,
+			// they are not wrapped text
+			// - If any consecutive following lines have the same number
+			// of quotes, they are not wrapped text
+			// - If any of the following lines have less quotes than the
+			// previous following lines, they are not wrapped text
+			// - If the following line has no characters, they are not
+			// wrapped text
+			for (int g = c + 1; g < readAhead; g++) {
+				chars = getLineAsChar(lines, g);
+				l.addLast(chars);
+
+				// rule 1
+				if (isQuoted(chars) && countQuotes(chars) > count) {
+					// System.err.println("Too many quotes - " + new
+					// String(chars));
+					l.removeLast();
+					break;
+				}
+				// rule 2
+				if (countWords(chars) > 2) {
+					// System.err.println("Too many words - " + new
+					// String(chars));
+					l.removeLast();
+					break;
+
+				}
+				// rule 3
+				if (countQuotes(chars) == count) {
+					// System.err.println("same number of quotes - " + new
+					// String(chars));
+					l.removeLast();
+					break;
+				}
+				// rule 4
+				if (!checkQuoteConsistency(l)) {
+					// System.err.println("Quote Inconsistent - " + new
+					// String(chars));
+					l.removeLast();
+					break;
+				}
+				// rule 5
+				if (new String(chars).trim().length() < 1) {
+					// System.err.println("Empty line - " + new String(chars));
+					l.removeLast();
+					break;
+				}
+			}
+
+			// we have wrapped text
+			if (l.size() > 0) {
+				Iterator iter = l.iterator();
+				StringBuilder sb = new StringBuilder(l.size() + 1 * 80);
+				String line = (String) lines.get(c);
+
+				if (isHTMLFilterActive) {
+					line = StringUtil.replace(line, ">", "&gt;");
+				}
+
+				sb.append(line);
+
+				while (iter.hasNext()) {
+					chars = (char[]) iter.next();
+					sb.append(" ");
+					sb.append(stripQuotes(chars));
+					lines.remove(c + 1);
+				}
+
+				lines.set(c, sb.toString().intern());
+			}
+		}
+	}
+
+	private char[] getLineAsChar(ArrayList lines, int number) {
+		String line = (String) lines.get(number);
+		return StringUtil.replace(line, "&gt;", ">").toCharArray();
+	}
+
+	private int countWords(char[] chars) {
+		String line = stripQuotes(chars).trim();
+		int count = 0;
+
+		for (int x = 0; x < line.length(); x++) {
+			if (line.charAt(x) == ' ') {
+				count++;
+			}
+		}
+
+		return count + 1;
+	}
+
+	private boolean checkQuoteConsistency(LinkedList l) {
+
+		for (int c = l.size() - 1; c > 0; c--) {
+			for (int g = 0; g < c; g++) {
+				if (countQuotes((char[]) l.get(c)) < countQuotes((char[]) l.get(g))) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	private boolean isQuoted(char[] chars) {
+		for (int c = 0; c < chars.length; c++) {
+			if (chars[c] == quoteChar) {
+				return true;
+			} else if (chars[c] != ' ') {
+				return false;
+			}
+		}
+
+		return false;
+	}
+
+	private int countQuotes(char[] chars) {
+		int quoteCount = 0;
+
+		for (int c = 0; c < chars.length; c++) {
+			if (chars[c] == quoteChar) {
+				quoteC
