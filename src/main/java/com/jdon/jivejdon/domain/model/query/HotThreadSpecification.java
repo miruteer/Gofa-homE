4faef398b0d094859a30cbb03c05d33b78c50e26
miruteer <117240226+miruteer@users.noreply.github.com>
2022-05-21 23:08:41 +0000

    rounded corners

---
+++ src/main/java/com/jdon/jivejdon/domain/model/query/HotThreadSpecification.java
@@ -0,0 +1,25 @@
+
+package com.jdon.jivejdon.domain.model.query;
+
+import com.jdon.jivejdon.spi.component.query.ThreadCompareVO;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class HotThreadSpecification {
+
+	public void sortByMessageCount(List<ThreadCompareVO> threads) {
+		Comparator comp = new ThreadReplyComparator();
+		Collections.sort(threads, Collections.reverseOrder(comp));
+	}
+
+	static class ThreadReplyComparator implements Comparator {
+		public int compare(Object x, Object y) {
+			ThreadCompareVO a = (ThreadCompareVO) x, b = (ThreadCompareVO) y;
+			int replya = a.getMessageCount();
+			int replyb = b.getMessageCount();
+			return replya - replyb;
+		}
+	}
+}
---
+++ src/main/java/com/jdon/jivejdon/domain/model/query/MessageSearchSpec.java
@@ -0,0 +1,19 @@
+package com.jdon.jivejdon.domain.model.query;
+
+import com.jdon.jivejdon.domain.model.ForumMessage;
+
+public class MessageSearchSpec {
+
+	private ForumMessage message;
+
+	private Long messageId;
+
+	private String subject;
+
+	private String body;
+
+	private int resultAllCount;
+
+	private boolean root;
+
+	public MessageSearc
