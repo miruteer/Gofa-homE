36af63b3b36e377441b8248c3a11cb27d5e515e7
miruteer <117240226+miruteer@users.noreply.github.com>
2022-05-27 08:40:02 +0000

    examples fixed

--- src/main/java/com/jdon/jivejdon/domain/model/realtime/NotificationLocatorTest.java
+++ src/main/java/com/jdon/jivejdon/domain/model/realtime/NotificationLocatorTest.java
@@ -6,4 +6,81 @@
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, softwar
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package com.jdon.jivejdon.domain.model.realtime;
+
+import junit.framework.TestCase;
+
+import com.jdon.jivejdon.domain.model.realtime.notification.ContentFormatConverter;
+
+public class NotificationLocatorTest extends TestCase {
+
+	private NotificationLocator notificationLocator;
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		notificationLocator = new NotificationLocator(new ContentFormatConverter("540", "ZH_CN", "", ""));
+
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testAddNotification() {
+		Notification notification = new Notification();
+		notification.setContent("bed");
+		notificationLocator.addNotification(notification);
+
+		notification = new Notification();
+		notification.setContent("987");
+		notificationLocator.addNotification(notification);
+
+		notification = new Notification();
+		notification.setContent("fgh");
+		notificationLocator.addNotification(notification);
+
+		notification = new Notification();
+		notification.setContent("12");
+		notificationLocator.addNotification(notification);
+
+	}
+
+	public void testCheckNotification() {
+		Notification notification = new Notification();
+		notification.setContent("new9");
+		notificationLocator.addNotification(notification);
+
+		notification = notificationLocator.checkNotification();
+		System.out.print("count =" + notification.getContent());
+
+		notification = new Notification();
+		notification.setContent("newfffff");
+		notificationLocator.addNotification(notification);
+
+		notification = new Notification();
+		notification.setContent("newggggggg");
+		notificationLocator.addNotification(notification);
+
+		notification = notificationLocator.checkNotification();
+		System.out.print("count =" + notification.getContent());
+
+		notification = notificationLocator.checkNotification();
+		System.out.print("count =" + notification.getContent());
+
+		notification = new Notification();
+		notification.setContent("newggggggg");
+		notificationLocator.addNotification(notification);
+
+		notification = notificationLocator.checkNotification();
+		System.out.print("count =" + notification.getContent());
+
+	}
+
+}
