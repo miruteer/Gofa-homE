ee02b0ccfa3c93f5c07566999764c4a0d42c75cf
miruteer <117240226+miruteer@users.noreply.github.com>
2022-05-27 18:37:04 +0000

    Updated the CHANGELOG.

---
+++ src/main/java/com/jdon/jivejdon/domain/model/reblog/ReBlogVO.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2003-2009 the original author or authors.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package com.jdon.jivejdon.domain.model.reblog;
+
+import java.util.Collection;
+
+import com.jdon.domain.message.DomainMessage;
+import com.jdon.jivejdon.spi.pubsub.reconstruction.LazyLoaderRole;
+import com.jdon.jivejdon.domain.model.ForumThread;
+import com.jdon.jivejdon.domain.model.util.LazyLoader;
+import com.jdon.jivejdon.domain.model.util.Many2ManyDTO;
+
+public class ReBlogVO extends LazyLoader {
+
+	private final long Id;
+
+	private LazyLoaderRole lazyLoaderRole;
+
+	private volatile Collection<ForumThread> threadTos;
+
+	private volatile Collection<ForumThread> threadFroms;
+
+	private volatile boolean load;
+
+	public ReBlogVO(long Id, LazyLoaderRole lazyLoaderRole) {
+		super();
+		this.Id = Id;
+		this.lazyLoaderRole
