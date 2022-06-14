8fa1e35224b7b507cbc5d9db551adce774067800
miruteer <117240226+miruteer@users.noreply.github.com>
2022-06-14 19:40:51 +0000

    Build dist

--- src/main/java/com/jdon/jivejdon/domain/model/thread/ViewCounter.java
+++ src/main/java/com/jdon/jivejdon/domain/model/thread/ViewCounter.java
@@ -20,4 +20,106 @@ import java.util.Objects;
 import com.jdon.domain.message.DomainMessage;
 import com.jdon.jivejdon.domain.model.ForumThread;
 import java.util.Queue;
-import com.go
+import com.google.common.collect.EvictingQueue;
+
+public class ViewCounter implements Comparable<ViewCounter> {
+
+	private final ForumThread thread;
+	private int viewCount = -1;
+	private int lastSavedCount;
+	Queue<String> fifo; 
+
+	public ViewCounter(ForumThread thread) {
+		this.thread = thread;
+		this.lastSavedCount = -1;
+		this.fifo = EvictingQueue.create(5); 
+	}
+
+	public void loadinitCount() {
+		if (this.viewCount != -1)
+			return;
+		DomainMessage dm = this.thread.lazyLoaderRole.loadViewCount(thread.getThreadId());
+		try {
+			// this.viewCount = 0;//flag it
+			Integer count = (Integer) dm.getEventResult();
+			if (count != null) {
+				this.viewCount = count;
+				this.lastSavedCount = count;
+				dm.clear();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public int getViewCount() {
+		if (this.viewCount == -1) {
+			loadinitCount();
+		}
+		return this.viewCount;
+	}
+
+	public boolean addViewCount(String ip) {
+		if (getViewCount() != -1){
+			if (!fifo.contains(ip)){
+			    viewCount++;
+		    	fifo.add(ip); 
+			    return true;
+		    }
+		}		
+		return false;
+	}
+
+	public boolean checkIP(String ip){
+		return fifo.contains(ip);
+	}
+
+	public int getLastSavedCount() {
+		return lastSavedCount;
+	}
+
+	public void setLastSavedCount(int lastSavedCount) {
+		this.lastSavedCount = lastSavedCount;
+	}
+
+	public ForumThread getThread() {
+		return thread;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		ViewCounter viewCounter = (ViewCounter) o;
+		if (this.thread.getThreadId() == null || viewCounter.getThread().getThreadId() == null)
+			return false;
+		return this.thread.getThreadId().longValue() == viewCounter.getThread().getThreadId().longValue();
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(this.thread.getThreadId());
+	}
+
+	@Override
+	public int compareTo(ViewCounter o) {
+		int diff1 = getViewCount() - getLastSavedCount();
+		int diff2 = o.getViewCount() - o.getLastSavedCount();
+		if (diff1 == diff2) {
+			if (thread.getThreadId() > o.getThread().getThreadId())
+				return -1;
+			else if (thread.getThreadId() < o.getThread().getThreadId())
+				return 1;
+		} else if (diff1 > diff2)
+			return -1;
+		else
+			return 1;
+		return 0;
+	}
+
+}
