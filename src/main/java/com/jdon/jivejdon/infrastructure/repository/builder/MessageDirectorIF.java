51d8ca18e1e4c1c67a6889958cce5a9931531a40
miruteer <117240226+miruteer@users.noreply.github.com>
2022-06-19 17:14:44 +0000

    netfilter: xt_qtaguid: fix memory leak in seq_file handlers

---
+++ src/main/java/com/jdon/jivejdon/infrastructure/repository/builder/MessageDirectorIF.java
@@ -0,0 +1,14 @@
+
+package com.jdon.jivejdon.infrastructure.repository.builder;
+
+import com.jdon.jivejdon.domain.model.ForumMessage;
+import com.jdon.jivejdon.domain.model.ForumThread;
+
+public interface MessageDirectorIF {
+
+    ForumMessage getMessage(Long messageId);
+
+    ForumMessage getRootMessage(Long messageId, ForumThread forumThread);
+
+    void setThreadDirector(ThreadDirectorIF threadDirectorIF);
+}
---
+++ src/main/java/com/jdon/jivejdon/infrastructure/repository/builder/ThreadRepositoryDao.java
@@ -0,0 +1,37 @@
+package com.jdon.jivejdon.infrastructure.repository.builder;
+
+import com.jdon.jivejdon.domain.model.ForumThread;
+import com.jdon.jivejdon.infrastructure.dto.AnemicMessageDTO;
+import com.jdon.jivejdon.infrastructure.repository.ThreadRepository;
+import com.jdon.jivejdon.infrastructure.repository.dao.MessageDaoFacade;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.List;
+
+public class ThreadRepositoryDao implements ThreadRepository {
+	private final static Logger logger = LogManager.getLogger(ThreadRepositoryDao.class);
+
+	protected MessageDaoFacade messageDaoFacade;
+
+	public ThreadRepositoryDao(MessageDaoFacade messageDaoFacade) {
+		this.messageDaoFacade = messageDaoFacade;
+	}
+
+	
+	public void createThread(AnemicMessageDTO forumMessagePostDTO) throws Exception {
+		messageDaoFacade.getMessageDao().createThread(forumMessagePostDTO);
+	}
+
+	/* (non-Javadoc)
+	 * @see com.jdon.jivejdon.infrastructure.repository.ThreadRepository#updateThread(com.jdon.jivejdon.domain.model
+	 * .ForumThread)
+	 */
+	public void updateThread(ForumThread thread) throws Exception {
+		messageDaoFacade.getMessageDao().updateThread(thread);
+		// thread.setModified(true);
+	}
+
+	/* (non-Javadoc)
+	 * @see com.jdon.jivejdon.infrastructure.repository.ThreadRepository#deleteThread(com.jdon.jivejdon.domain.model
+	 * .Fo
