ac2c5ae48b216f9ffd103618243f02758730db93
miruteer <117240226+miruteer@users.noreply.github.com>
2022-07-22 13:27:23 +0000

    Fixing the build.

--- src/main/java/com/jdon/jivejdon/infrastructure/repository/dao/sql/TagDaoSql.java
+++ src/main/java/com/jdon/jivejdon/infrastructure/repository/dao/sql/TagDaoSql.java
@@ -20,4 +20,125 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.jdon.controller.model.PageIterator;
+import com.jdon.jivejdon.util.Constants;
+import com.jdon.jivejdon.domain.model.property.ThreadTag;
+import com.jdon.jivejdon.domain.model.query.specification.TaggedThreadListSpec;
+import com.jdon.jivejdon.infrastructure.repository.dao.SequenceDao;
+import com.jdon.jivejdon.infrastructure.repository.dao.TagDao;
+import com.jdon.jivejdon.util.ContainerUtil;
+import com.jdon.model.query.PageIteratorSolver;
+
+public class TagDaoSql implements TagDao {
+	private final static Logger logger = LogManager.getLogger(TagDaoSql.class);
+
+	private PageIteratorSolver pageIteratorSolver;
+
+	private JdbcTempSource jdbcTempSource;
+
+	private SequenceDao sequenceDao;
+
+	public TagDaoSql(JdbcTempSource jdbcTempSource, ContainerUtil containerUtil, SequenceDao sequenceDao) {
+		this.pageIteratorSolver = new PageIteratorSolver(jdbcTempSource.getDataSource(), containerUtil.getCacheManager());
+		this.jdbcTempSource = jdbcTempSource;
+		this.sequenceDao = sequenceDao;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.jdon.jivejdon.dao.sql.TagDao#createThreadTag(com.jdon.jivejdon.domain.model
+	 * .ThreadTag)
+	 */
+	public void createThreadTag(ThreadTag threadTag) throws Exception {
+		try {
+			String ADD_SQL = "INSERT INTO tag(tagID, title, assonum)" + " VALUES (?,?,?)";
+			List queryParams = new ArrayList();
+			queryParams.add(threadTag.getTagID());
+			queryParams.add(threadTag.getTitle());
+			queryParams.add(threadTag.getAssonum());
+
+			jdbcTempSource.getJdbcTemp().operate(queryParams, ADD_SQL);
+			clearCache();
+		} catch (Exception e) {
+			logger.error(e);
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.jdon.jivejdon.dao.sql.TagDao#delThreadTag(com.jdon.jivejdon.domain.model
+	 * .ForumThread)
+	 */
+	public void delThreadTag(Long threadID) throws Exception {
+		String SQL = "DELETE FROM threadTag WHERE threadID=?";
+		List queryParams = new ArrayList();
+		queryParams.add(threadID);
+		jdbcTempSource.getJdbcTemp().operate(queryParams, SQL);
+		clearCache();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.jdon.jivejdon.dao.sql.TagDao#addThreadTag(com.jdon.jivejdon.domain.model
+	 * .ThreadTag, com.jdon.jivejdon.domain.model.ForumThread)
+	 */
+	public void addThreadTag(Long tagID, Long threadID) throws Exception {
+		String SQL = "INSERT INTO threadTag(threadTagID, threadID, tagID)" + " VALUES (?,?,?)";
+		List queryParams = new ArrayList();
+		Long threadTagID = sequenceDao.getNextId(Constants.OTHERS);
+		queryParams.add(threadTagID);
+		queryParams.add(threadID);
+		queryParams.add(tagID);
+		jdbcTempSource.getJdbcTemp().operate(queryParams, SQL);
+		clearCache();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.jdon.jivejdon.dao.sql.TagDao#delThreadTag(com.jdon.jivejdon.domain.model
+	 * .ThreadTag, com.jdon.jivejdon.domain.model.ForumThread)
+	 */
+	public void delThreadTag(Long tagID, Long threadID) throws Exception {
+		String SQL = "DELETE FROM threadTag WHERE threadID=? and tagID =?";
+		List queryParams = new ArrayList();
+		queryParams.add(threadID);
+		queryParams.add(tagID);
+		jdbcTempSource.getJdbcTemp().operate(queryParams, SQL);
+		clearCache();
+	}
+
+	public void deleteThreadTag(Long tagID) throws Exception {
+		String SQL = "DELETE FROM tag WHERE  tagID =?";
+		List queryParams = new ArrayList();
+		queryParams.add(tagID);
+		jdbcTempSource.getJdbcTemp().operate(queryParams, SQL);
+
+		String SQL2 = "DELETE FROM threadTag WHERE  tagID =?";
+		List queryParams2 = new ArrayList();
+		queryParams2.add(tagID);
+		jdbcTempSource.getJdbcTemp().operate(queryParams2, SQL2);
+
+		clearCache();
+	}
+
+	public boolean checkThreadTagRelation(Long tagID, Long threadID) {
+		String LOAD_SQL = "SELECT threadTagID FROM threadTag WHERE threadID=? and tagID =?";
+		List queryParams = new ArrayList();
+		queryParams.add(threadID);
+		queryParams.add(tagID);
+		boolean ret = false;
+		try {
+			List list
