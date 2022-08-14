808ea947d62c4a1cde34c5e1768939163d0fa42d
miruteer <117240226+miruteer@users.noreply.github.com>
2022-08-14 10:05:31 +0000

    grrrrr

--- src/main/java/com/jdon/jivejdon/infrastructure/repository/subscription/SubscriptionInitFactory.java
+++ src/main/java/com/jdon/jivejdon/infrastructure/repository/subscription/SubscriptionInitFactory.java
@@ -17,4 +17,80 @@ package com.jdon.jivejdon.infrastructure.repository.subscription;
 
 import com.jdon.annotation.Component;
 import com.jdon.jivejdon.util.Constants;
-import com.jdon.jivejdon.spi.component.subscription.Subscripti
+import com.jdon.jivejdon.spi.component.subscription.SubscriptionAction;
+import com.jdon.jivejdon.spi.component.subscription.action.EmailAction;
+import com.jdon.jivejdon.spi.component.subscription.action.ShortMsgAction;
+import com.jdon.jivejdon.spi.component.subscription.action.SinaWeiboAction;
+import com.jdon.jivejdon.spi.component.weibo.UserConnectorAuth;
+import com.jdon.jivejdon.domain.model.account.Account;
+import com.jdon.jivejdon.domain.model.subscription.Subscription;
+import com.jdon.jivejdon.domain.model.subscription.messsage.WeiboExpiredNotifyMessage;
+import com.jdon.jivejdon.infrastructure.repository.dao.sql.UserconnectorSql;
+
+import java.util.Map;
+
+@Component
+public class SubscriptionInitFactory {
+	public static final String SINAWEIBO = "sinaweibo";
+
+	protected Constants constants;
+	private UserconnectorSql userconnectorSql;
+	private WeiboExpiredNotifyMessage weiboExpiredNotifyMessage;
+
+	public SubscriptionInitFactory(Constants constants, UserconnectorSql userconnectorSql, WeiboExpiredNotifyMessage weiboExpiredNotifyMessage) {
+		super();
+		this.constants = constants;
+		this.userconnectorSql = userconnectorSql;
+		this.weiboExpiredNotifyMessage = weiboExpiredNotifyMessage;
+	}
+
+	public SubscriptionAction create(String notifymode) {
+		SubscriptionAction subscriptionAction = null;
+		if (notifymode.equals(SINAWEIBO)) {
+			subscriptionAction = new SinaWeiboAction(weiboExpiredNotifyMessage);
+		}
+		return subscriptionAction;
+
+	}
+
+	public Subscription populateSubscription(Map map) {
+		Subscription subscription = new Subscription();
+		subscription.setSubscriptionId((Long) map.get("subscriptionID"));
+
+		String userId = (String) map.get("userId");
+		Account accountin = new Account();
+		accountin.setUserId(userId);
+		subscription.setAccount(accountin);
+
+		Integer subscribeType = (Integer) map.get("subscribedtype");
+		Long subscribeID = (Long) map.get("subscribedID");
+
+		subscription.setSubscribed(SubscribedFactory.createTransient(subscribeType.intValue(), subscribeID));
+
+		String saveDateTime = ((String) map.get("creationDate")).trim();
+		subscription.setCreationDate(constants.getDateTimeDisp(saveDateTime));
+
+		Boolean sendmsg = (Boolean) map.get("sendmsg");
+		if (sendmsg)
+			subscription.addAction(new ShortMsgAction());
+
+		Boolean sendemail = (Boolean) map.get("sendemail");
+		if (sendemail) {
+			subscription.addAction(new EmailAction(subscription));
+		}
+
+		String notifymode = (String) map.get("notifymode");
+		subscription.setNotifymode(notifymode);
+		if (notifymode != null && notifymode.length() != 0) {
+			UserConnectorAuth userConnectorAuth = userconnectorSql.loadUserConnectorAuth(userId, notifymode);
+			SubscriptionAction subscriptionAction = create(notifymode);
+			if (userConnectorAuth != null && !userConnectorAuth.isEmpty() && subscriptionAction != null) {
+				subscriptionAction.setUserConnectorAuth(userConnectorAuth);
+				subscriptionAction.setSubscription(subscription);
+				subscription.addAction(subscriptionAction);
+			}
+		}
+
+		return subscription;
+	}
+}
