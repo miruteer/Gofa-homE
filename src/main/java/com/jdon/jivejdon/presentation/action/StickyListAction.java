02ffa1a6f3e02fd9228eca69b013fdb3eeb74986
miruteer <117240226+miruteer@users.noreply.github.com>
2022-08-21 10:46:36 +0000

    fix object reference error

---
+++ src/main/java/com/jdon/jivejdon/presentation/action/StickyListAction.java
@@ -0,0 +1,13 @@
+package com.jdon.jivejdon.presentation.action;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.Ac
