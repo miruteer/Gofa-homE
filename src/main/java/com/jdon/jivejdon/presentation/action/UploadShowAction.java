d8a5dfccb2bdbdab16d1879457ee2f642526e81b
miruteer <117240226+miruteer@users.noreply.github.com>
2022-08-26 06:52:07 +0000

    libata: Add helper to determine when PHY events should be ignored

--- src/main/java/com/jdon/jivejdon/presentation/action/UploadShowAction.java
+++ src/main/java/com/jdon/jivejdon/presentation/action/UploadShowAction.java
@@ -24,4 +24,115 @@ import javax.servlet.http.HttpSession;
 
 import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
-import org.apach
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+import com.jdon.controller.WebAppUtil;
+import com.jdon.jivejdon.spi.component.throttle.hitkey.CustomizedThrottle;
+import com.jdon.jivejdon.spi.component.throttle.hitkey.HitKeyIF;
+import com.jdon.jivejdon.spi.component.throttle.hitkey.HitKeySame;
+import com.jdon.jivejdon.domain.model.attachment.UploadFile;
+import com.jdon.jivejdon.domain.model.message.upload.UploadFileFilter;
+import com.jdon.jivejdon.api.property.UploadService;
+import com.jdon.util.Debug;
+import com.jdon.util.UtilValidate;
+
+/**
+ * id and oid are need oid is parentId of the image id is the image id;
+ * 
+ * @author banq(http://www.jdon.com)
+ * 
+ */
+public class UploadShowAction extends Action {
+	public final static String module = UploadShowAction.class.getName();
+	private static final byte[] BLANK = { 71, 73, 70, 56, 57, 97, 1, 0, 1, 0, -111, 0, 0, 0, 0, 0, -1, -1, -1, -1, -1, -1, 0, 0, 0, 33, -7, 4, 1, 0,
+			0, 2, 0, 44, 0, 0, 0, 0, 1, 0, 1, 0, 0, 2, 2, 76, 1, 0, 59 };
+
+	private CustomizedThrottle customizedThrottle;
+
+	public ActionForward execute(ActionMapping actionMapping, ActionForm actionForm, HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+
+		long header = request.getDateHeader("If-Modified-Since");
+		if (header > 0) {
+			response.setStatus(HttpServletResponse.SC_NOT_MODIFIED);
+			return null;
+		}
+
+		String dlname = request.getParameter(UploadFileFilter.DOWNLOAD_NAME);
+		String id = request.getParameter("id");
+		String oid = request.getParameter("oid");
+		String type = request.getParameter("type");
+
+		try {
+			if (UtilValidate.isEmpty(id) && UtilValidate.isEmpty(oid))
+				throw new Exception("parameter id or oid is null");
+
+			if (!checkSpamHit(id, request)) {
+				throw new Exception("this is spam =" + request.getRemoteAddr());
+			}
+
+			UploadFile uploadFile = getUploadFile(oid, id);
+
+			if (uploadFile == null)
+				throw new Exception("uploadFile == null");
+
+			if (!UtilValidate.isEmpty(uploadFile.getContentType()))
+				type = uploadFile.getContentType();
+			else
+				type = "image/JPEG";
+
+			byte[] bytes = null;
+			if (!UtilValidate.isEmpty(oid))
+				bytes = uploadFile.getData(oid);
+			else
+				// for old version
+				bytes = uploadFile.getData();
+
+			if (bytes == null) {
+				bytes = BLANK;
+			}
+			outUploadFile(response, bytes, type, dlname);
+		} catch (Exception ex) {
+			Debug.logError(
+					"when get the image, error happened:" + ex.getMessage() + " id=" + id + " oid=" + oid + " remote=" + request.getRemoteAddr(),
+					module);
+			closeResponse(request, response);
+		}
+		return null;
+	}
+
+	protected UploadFile getUploadFile(String oid, String id) {
+		if (UtilValidate.isEmpty(id))
+			return null;
+		UploadService uploadService = (UploadService) WebAppUtil.getService("uploadService", this.servlet.getServletContext());
+		return uploadService.getUploadFile(id);
+	}
+
+	private void closeResponse(HttpServletRequest request, HttpServletResponse response) {
+		try {
+			disableSessionOnlines(request);
+			HttpServletResponse httpResponse = (HttpServletResponse) response;
+			httpResponse.sendError(HttpServletResponse.SC_FORBIDDEN);
+		} catch (IOException e) {
+		}
+	}
+
+	private void disableSessionOnlines(HttpServletRequest httpRequest) {
+		HttpSession session = httpRequest.getSession(false);
+		if (session != null)
+			session.invalidate();
+	}
+
+	private boolean checkSpamHit(String oid, HttpServletRequest request) {
+		if (customizedThrottle == null) {
+			customizedThrottle = (CustomizedThrottle) WebAppUtil.getComponentInstance("customizedThrottle", this.servlet.getServletContext());
+		}
+		HitKeyIF hitKey = new HitKeySame(request.getRemoteAddr(), oid);
+		return customizedThrottle.processHitFilter(hitKey);
+	}
+
+	protected void outUploadFile(HttpServletResponse response, byte[] data, String type, String dlname) throws Exception {
+		if (data == null)
+			throw new Exception("data is null ");
+	
