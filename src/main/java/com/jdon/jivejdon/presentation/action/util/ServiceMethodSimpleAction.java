a7436ca6f035aae4cd74adcadf93984ae1b46224
miruteer <117240226+miruteer@users.noreply.github.com>
2022-09-13 21:54:17 +0000

    Update PNaCl_Csound_04_RealTime3DScoreGenerator.html

--- src/main/java/com/jdon/jivejdon/presentation/action/util/ServiceMethodSimpleAction.java
+++ src/main/java/com/jdon/jivejdon/presentation/action/util/ServiceMethodSimpleAction.java
@@ -23,4 +23,39 @@ import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
 import com.jdon.controller.WebAppUtil;
-import com.jdon.strutsutil.FormBeanUtil
+import com.jdon.strutsutil.FormBeanUtil;
+import com.jdon.strutsutil.ModelBaseAction;
+
+public class ServiceMethodSimpleAction extends ModelBaseAction {
+
+	public ActionForward execute(ActionMapping actionMapping, ActionForm actionForm, HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		String service = request.getParameter("service");
+		if (service == null) {
+			return actionMapping.findForward("failure");
+		}
+		String method = request.getParameter("method");
+		if (method == null) {
+			return actionMapping.findForward("failure");
+		}
+
+		Object[] methodParas = new Object[] {};
+		String id = request.getParameter("id");
+		if (id != null)
+			methodParas = new Object[] { Long.parseLong(id) };
+
+		Object result = WebAppUtil.callService(service, method, methodParas, request);
+		if (result != null)
+			request.setAttribute(result.getClass().getSimpleName(), result);
+		ActionForward sucessaf = actionMapping.findForward(FormBeanUtil.FORWARD_SUCCESS_NAME);
+		ActionForward actionnameaf = actionMapping.findForward(method);
+		if (sucessaf != null) {
+			return sucessaf;
+		} else if (actionnameaf != null) {
+			return actionnameaf;
+		} else {
+			return null;
+		}
+	}
+
+}
