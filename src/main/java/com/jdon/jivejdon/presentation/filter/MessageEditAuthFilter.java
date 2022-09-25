b082b03703c2723f058f268cbc6663219f079366
miruteer <117240226+miruteer@users.noreply.github.com>
2022-09-25 11:24:13 +0000

    RAL integration: WIP

---
+++ src/main/java/com/jdon/jivejdon/presentation/filter/MessageEditAuthFilter.java
@@ -0,0 +1,65 @@
+package com.jdon.jivejdon.presentation.filter;
+
+import com.jdon.controller.WebAppUtil;
+import com.jdon.jivejdon.domain.model.Forum;
+import com.jdon.jivejdon.domain.model.ForumMessage;
+import com.jdon.jivejdon.domain.model.ForumThread;
+import com.jdon.jivejdon.domain.model.account.Account;
+import com.jdon.jivejdon.presentation.form.MessageForm;
+import com.jdon.jivejdon.api.account.AccountService;
+import com.jdon.jivejdon.api.ForumMessageService;
+import com.jdon.jivejdon.api.ForumService;
+import com.jdon.util.Debug;
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Collection;
+
+/**
+ * the filter for Message edit Action,
+ *
+ * @author banq
+ *
+ */
+public class MessageEditAuthFilter extends Action {
+
+	public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+		Debug.logVerbose("enter MessageEditAuthFilter");
+		MessageForm messageForm = (MessageForm) form;
+		AccountService accountService = (AccountService) WebAppUtil.getService("accountService", request);
+		Account account = accountService.getloginAccount();
+		if (account == null) {
+			messageForm.setAuthenticated(false);// only logined user can post
+			return mapping.findForward("failure");
+		}
+		if ((messageForm.getAction() != null) && (!messageForm.getAction().equals(MessageForm.CREATE_STR))) {
+			ForumMessageService forumMessageService = (ForumMessageService) WebAppUtil.getService("forumMessageService", request);
+			ForumMessage forumMessage = null;
+			if (messageForm.getMessageId() != null)
+			 forumMessage = forumMessageService.getMessage(messageForm.getMessageId());
+			else if (messageForm.getForumThread().getThreadId() != null){
+				ForumThread forumThread = forumMessageService.getThread(new
+						Long(messageForm.getForumThread().getThreadId()));
+				if (forumThread != null)
+					forumMessage = forumThread.getRootMessage();
+			}
+			boolean result = forumMessageService.checkIsAuthenticated(forumMessage);
+			messageForm.setAuthenticated(result);
+		}
+		ForumService forumService = (ForumService) WebAppUtil.getService("forumService", this.servlet.getServletContext());
+		Collection<Forum> forums = forumService.getForums();
+		request.setAttribute("forums", forums);
+		return mapping.findForward(messageForm.getAction());
+	}
+
+	protected boolean beReblog() {
+		boolean ret = false;
+
+		return ret;
+	}
+
+}
