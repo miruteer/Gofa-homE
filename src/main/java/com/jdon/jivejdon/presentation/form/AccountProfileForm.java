666db22ccc74e5743423ebd47fee3099637b34fa
miruteer <117240226+miruteer@users.noreply.github.com>
2022-10-09 18:50:35 +0000

    update styling

--- src/main/java/com/jdon/jivejdon/presentation/form/AccountProfileForm.java
+++ src/main/java/com/jdon/jivejdon/presentation/form/AccountProfileForm.java
@@ -26,4 +26,78 @@ public class AccountProfileForm extends BaseForm {
 
 	private Collection propertys;
 
-	private int max
+	private int maxSize = 20;
+
+	private int validateCode;
+
+	public AccountProfileForm() {
+		account = new Account();
+		propertys = new ArrayList();
+		for (int i = 0; i < maxSize; i++) {
+			propertys.add(new Property());
+		}
+	}
+
+	public String getSignature() {
+		return signature;
+	}
+
+	public void setSignature(String signature) {
+		this.signature = signature;
+	}
+
+	public Collection getPropertys() {
+		return propertys;
+	}
+
+	public void setPropertys(Collection propertys) {
+		this.propertys = propertys;
+	}
+
+	public Property getProperty(int index) {
+		Property prop = (Property) ((List) propertys).get(index);
+		if (prop == null)
+			return null;
+		if (prop.getValue() == null)
+			return prop;
+
+		if (!prop.getValue().contains("<a href=")) {
+			String newString = prop.getValue().replaceAll("(?:https?|ftps?)://[\\w/%.-]+", "<a href='$0'>$0</a>");
+			prop.setValue(newString);
+		}
+		return prop;
+	}
+
+	public Account getAccount() {
+		return account;
+	}
+
+	public void setAccount(Account account) {
+		this.account = account;
+	}
+
+	public void doValidate(ActionMapping mapping, HttpServletRequest request, List errors) {
+		if (getPropertys().size() > maxSize) {
+			errors.add("max length is " + maxSize);
+			return;
+		}
+
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public int getValidateCode() {
+		return validateCode;
+	}
+
+	public void setValidateCode(int validateCode) {
+		this.validateCode = validateCode;
+	}
+
+}
