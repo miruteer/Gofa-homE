a4894dc4d046c6e2c62fb68383625c122e4d353a
miruteer <117240226+miruteer@users.noreply.github.com>
2022-10-16 22:21:34 +0000

    Updating the version number

--- src/main/java/com/jdon/jivejdon/presentation/servlet/VerifySpammerAction.java
+++ src/main/java/com/jdon/jivejdon/presentation/servlet/VerifySpammerAction.java
@@ -11,4 +11,35 @@ import org.apache.logging.log4j.*;
 
 import com.jdon.controller.WebAppUtil;
 import com.jdon.jivejdon.spi.component.throttle.hitkey.CustomizedThrottle;
-import com.jdon.jivejdon.presentatio
+import com.jdon.jivejdon.presentation.form.SkinUtils;
+
+public class VerifySpammerAction extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private final static Logger logger = LogManager.getLogger(VerifySpammerAction.class);
+
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		logger.debug("enter VerifySpammerAction");
+
+		String registerCode = request.getParameter("registerCode");
+		if (registerCode == null || registerCode.length() == 0) {
+			response.sendError(404);
+		}
+
+		if (SkinUtils.verifyRegisterCode(registerCode, request)) {
+			CustomizedThrottle customizedThrottle = (CustomizedThrottle) WebAppUtil
+					.getComponentInstance("customizedThrottle", this.getServletContext());
+			customizedThrottle.removeBanned(request.getRemoteAddr());
+			response.setHeader("Pragma", "No-cache");
+			response.setHeader("Cache-Control", "no-store");
+			response.setDateHeader("Expires", 0);
+			response.sendRedirect(request.getContextPath());
+			return;
+		}
+		response.sendError(404);
+
+	}
+}
